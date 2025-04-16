0eeff933224275e988fe8815d782ee1e0e137ee5
Donthulashivakumar <donthulashivakumar931@gmail.com>
2025-04-16 07:49:42 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,6 @@
+original_list = list(range(1, 11))
+print(f"Original list: {original_list}")
+extracted_elements = original_list[:5]
+print(f"Extracted first five elements: {extracted_elements}")
+reversed_elements = extracted_elements[::-1]
+print(f"Reversed extracted elements: {reversed_elements}")
